71fc61442baa0fa1d447bb43a964e0fae7d860e2
AlexandreSJ <alexandre.sj22@gmail.com>
2025-09-15 21:42:45 -0300

    fix: update server listen method to use environment variables for port and domain

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,12 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
 
 app.get('/', (_req, res) => {
   res.status(200).json({ message: 'It works' });
 });
 
-app.listen(port, () => {
-  console.log(`Server now running on: http://localhost:${port}`);
+app.listen(Number(process.env.PORT), process.env.DOMAIN, (err) => {
+  if(err) console.error(err);
+  console.log(`Server now running on: ${process.env.DOMAIN}:${process.env.PORT}`);
 });
