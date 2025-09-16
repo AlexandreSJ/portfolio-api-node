34163db5f5e1930d8f8794ed0472a92bc2271ea1
AlexandreSJ <alexandre.sj22@gmail.com>
2025-09-15 21:57:29 -0300

    fix: remove unnecessary domain parameter from server listen method

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ app.get('/', (_req, res) => {
   res.status(200).json({ message: 'It works' });
 });
 
-app.listen(Number(process.env.PORT), process.env.DOMAIN, (err) => {
+app.listen(process.env.PORT, (err) => {
   if(err) console.error(err);
   console.log(`Server now running on: ${process.env.DOMAIN}:${process.env.PORT}`);
 });
